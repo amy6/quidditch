32c31d8b053a5074f3bbaef680d8e9a49a0867b5
mahima <mahima.6.ms@gmail.com>
2018-06-09 19:49:23 +0530

    remove MaterialTapTargetPrompt library for submission as ABND project add comments

--- app/src/main/java/com/example/android/quidditch/MainActivity.java
+++ app/src/main/java/com/example/android/quidditch/MainActivity.java
@@ -1,177 +1,63 @@
 package com.example.android.quidditch;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
-import uk.co.samuelwall.materialtaptargetprompt.MaterialTapTargetPrompt;
-import uk.co.samuelwall.materialtaptargetprompt.extras.focals.RectanglePromptFocal;
-
 import static android.R.color;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView gryffindor_score_display, slytherin_score_display, winner_display;
-    private Button resetButton, slytherin_foul_button, gryffindor_quaffle_button, gryffindor_snitch_button;
-    private Button gryffindor_foul_button, slytherin_quaffle_button, slytherin_snitch_button;
-    private ArrayList<Button> buttonList;
+    //declare score variables
     private int gryffindor_score, slytherin_score;
+    //flag to hold if a new match is being started
     private boolean snitchCaught;
+
+    //declare references for views in the layout
+    private TextView gryffindor_score_display, slytherin_score_display, winner_display;
+    private Button resetButton;
+
+    //toast to display when a new games in started
     private Toast toast;
-    private int count;
-    private boolean firstUsage;
-    private final Handler handler = new Handler();
-    private final long delay = 300;
-    private long longDelay = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //obtain references for the score textviews
         gryffindor_score_display = findViewById(R.id.gryffindor_score);
         slytherin_score_display = findViewById(R.id.slytherin_score);
 
-        winner_display = findViewById(R.id.winner);
-
-        toast = new Toast(this);
-
-        setUpListOfButtons();
-
-        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        firstUsage = sharedPref.getBoolean(getString(R.string.setup_onboard), true);
-
-        if(firstUsage) {
-
-            for (Button button : buttonList) {
-                button.setClickable(false);
-            }
-
-            cancelToast();
-            toast = Toast.makeText(this, R.string.welcome_text, Toast.LENGTH_SHORT);
-            toast.show();
-
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    setUpWalkThrough(gryffindor_quaffle_button.getId(), R.string.sv_quaffle_content);
-                }
-            }, longDelay);
-
-        }
-
-
-    }
-
-    private void cancelToast() {
-        if(toast != null) {
-            toast.cancel();
-        }
-    }
-
-    private void setUpListOfButtons() {
-        buttonList = new ArrayList<>();
+        //obtain reference for the reset button which resets the scores
         resetButton = findViewById(R.id.reset_button);
-        slytherin_foul_button = findViewById(R.id.slytherin_foul);
-        gryffindor_quaffle_button = findViewById(R.id.gryffindor_quaffle);
-        gryffindor_snitch_button = findViewById(R.id.gryffindor_snitch);
-        gryffindor_foul_button = findViewById(R.id.gryffindor_foul);
-        slytherin_quaffle_button = findViewById(R.id.slytherin_quaffle);
-        slytherin_snitch_button = findViewById(R.id.slytherin_snitch);
 
-        buttonList.add(resetButton);
-        buttonList.add(slytherin_foul_button);
-        buttonList.add(gryffindor_quaffle_button);
-        buttonList.add(gryffindor_snitch_button);
-        buttonList.add(gryffindor_foul_button);
-        buttonList.add(slytherin_quaffle_button);
-        buttonList.add(slytherin_snitch_button);
+        //obtain reference for winner textview
+        winner_display = findViewById(R.id.winner);
     }
 
-    private void setUpWalkThrough(int viewID, int stringID) {
-
-        new MaterialTapTargetPrompt.Builder(this)
-                .setTarget(findViewById(viewID))
-                .setPrimaryText(stringID)
-                .setPromptStateChangeListener(new MaterialTapTargetPrompt.PromptStateChangeListener()
-                {
-                    @Override
-                    public void onPromptStateChanged(@NonNull MaterialTapTargetPrompt prompt, int state)
-                    {
-
-                        if (state == MaterialTapTargetPrompt.STATE_FOCAL_PRESSED
-                                || state == MaterialTapTargetPrompt.STATE_NON_FOCAL_PRESSED)
-                        {
-                            count++;
-                            switch (count) {
-                                case 1:
-                                    handler.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            setUpWalkThrough(slytherin_foul_button.getId(), R.string.sv_foul_content);
-                                        }
-                                    }, delay);
-                                    break;
-
-                                case 2:
-                                    handler.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            setUpWalkThrough(gryffindor_snitch_button.getId(), R.string.sv_snitch_content);
-                                        }
-                                    }, delay);
-                                    break;
-
-                                case 3:
-                                    handler.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            setUpWalkThrough(resetButton.getId(), R.string.sv_reset_content);
-                                        }
-                                    }, delay);
-                                    firstUsage = false;
-                                    break;
-
-                                default:
-                                    handler.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            for (Button button : buttonList) {
-                                                button.setClickable(true);
-                                            }
-                                        }
-                                    }, delay);
-
-                            }
-
-                        }
-                    }
-                })
-                .setBackgroundColour(ContextCompat.getColor(this, R.color.colorPrimary))
-                .setPromptFocal(new RectanglePromptFocal())
-                .show();
-
-
-    }
-
+    /**
+     * handle button clicks to update or reset scores
+     *
+     * @param view reference to the button view that generated the click event
+     */
     public void onClick(View view) {
 
+        //verify if the snitch has been caught in the previous match - if yes, display a toast to the user
+        // saying a new game has been started and reset the previous scores
         if (snitchCaught) {
+            //cancel any outstanding toast before displaying the new toast
             cancelToast();
             toast = Toast.makeText(this, getString(R.string.new_game_text), Toast.LENGTH_SHORT);
             toast.show();
             resetScore(resetButton);
         }
+
+        //get the ID of the button clicked and update the score for the respective team - a quaffle wins 10 points,
+        // a foul gets the opponent 5 points, snitch awards 150 points and ends the game
         int id = view.getId();
         switch (id) {
 
@@ -202,13 +88,17 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
+        //display the updated score
         displayScore();
     }
 
 
+    /**
+     * reset the score of both teams and display the updated score - clear the winner text, if any
+     *
+     * @param view reference to the reset button
+     */
     public void resetScore(View view) {
-
-
         gryffindor_score = 0;
         slytherin_score = 0;
         snitchCaught = false;
@@ -216,6 +106,9 @@ public class MainActivity extends AppCompatActivity {
         winner_display.setVisibility(View.GONE);
     }
 
+    /**
+     * displays the updated score - score with leading points is displayed in green, opponent team's score is displayed in red
+     */
     private void displayScore() {
         gryffindor_score_display.setText(String.valueOf(gryffindor_score));
         slytherin_score_display.setText(String.valueOf(slytherin_score));
@@ -231,11 +124,15 @@ public class MainActivity extends AppCompatActivity {
             slytherin_score_display.setTextColor(getResources().getColor(color.black));
         }
 
+        //if the snitch has been caught, game ends - display the user before a new game can begin
         if (snitchCaught)
             displayWinner();
 
     }
 
+    /**
+     * verify the winner, display the name and conclude the game
+     */
     private void displayWinner() {
         String winnerText = getString(R.string.snitch_caught_text);
         winnerText += (gryffindor_score > slytherin_score ? getString(R.string.gryffindor) : getString(R.string.slytherin));
@@ -247,12 +144,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-
+        //cancel any outstanding toast when the user exits the app
         cancelToast();
+    }
 
-        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(getString(R.string.setup_onboard), firstUsage);
-        editor.apply();
+    /**
+     * cancel any outstanding toasts - method will be called prior to displaying any new toasts
+     */
+    private void cancelToast() {
+        if (toast != null) {
+            toast.cancel();
+        }
     }
 }
